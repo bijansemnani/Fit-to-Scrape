8beb330369c46f06bdc7be21dcb9d338e7a273a0
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-04-09 20:12:25 -0700

    print all the articles on the html page

--- routes/api-scrape-routes.js
+++ routes/api-scrape-routes.js
@@ -1,12 +1,12 @@
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("../models");
-
+var resultArray = [];
 module.exports = function(app) {
+
   app.get("/scrape", function(req, res) {
     axios.get("https://www.reddit.com/").then(function(response) {
       var $ = cheerio.load(response.data);
-
       $("div.thing").each(function(i, element) {
         var result = {};
 
@@ -18,14 +18,25 @@ module.exports = function(app) {
 
         db.Article.create(result)
           .then(function(dbArticle) {
-
+            resultArray.push(dbArticle);
           })
           .catch(function(err) {
             res.json(err);
           });
       });
+    });
+    res.end();
+  });
 
-      res.end();
+  app.get("/articles", function (req, res) {
+    db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
     });
   });
 };
