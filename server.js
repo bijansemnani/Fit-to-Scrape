df3ea3bc0b134803e86fac7104db625e20eeaef3
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-04-11 18:26:04 -0700

    changed again

--- server.js
+++ server.js
@@ -30,12 +30,16 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var databaseUri = "mongodb://localhost/mongoHeadlines";
+
+if (process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
 
 require("./routes/api-scrape-routes.js")(app);
 require("./routes/api-saved-routes.js")(app);
